ae08b63c0e70a1a5f69c77e323be949ddb105c11
Elisha Kim <elishakkim@gmail.com>
2020-05-21 15:19:21 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Home from './components/home';
+import Header from './components/header';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header>
+          <Header />
+        </header>
+
+        <Switch>
+          <Route>
+            <Home path='/' exact/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navbar, NavbarBrand, Nav, NavLink } from 'react-bootstrap';
+import './style.scss'
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg='light' variant='navbar' expand='lg' className='navbar'>
+        <NavbarBrand>족과의 동침</NavbarBrand>
+        <Nav variant='tabs' justify={true} fill={true}>
+          <NavLink>Home</NavLink>
+          <NavLink>About Us</NavLink>
+        </Nav>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Header
