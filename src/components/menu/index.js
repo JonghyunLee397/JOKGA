12f7e85f59ea5bb1b38b2a5c5773c5f68badf6b0
Elisha Kim <elishakkim@gmail.com>
2020-05-21 19:52:01 -0400

    Removing Carousel from Menu

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,19 +1,10 @@
 import React, { useState } from 'react';
-import { Carousel, CarouselItem } from 'react-bootstrap';
 
 const Menu = () => {
-  const [index, setIndex] = useState(0)
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
 
   return (
     <div>
-      <Carousel activeIndex={index} onSelect={handleSelect}>
-        <CarouselItem>
-        
-        </CarouselItem>
-      </Carousel>
+      Menu
     </div>
   )
 }
