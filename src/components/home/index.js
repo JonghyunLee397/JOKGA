4d0faf13ce48587797badccaebab2a38db1a4f3b
Elisha Kim <elishakkim@gmail.com>
2020-05-22 01:28:24 -0400

    Adding react-icons, location picture

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,13 +1,30 @@
 import React, { useState } from 'react';
-import ImageCarousel from './carousel'
-import './style.scss'
+import { Button } from 'react-bootstrap';
+import { IoMdArrowForward } from 'react-icons/io'
+import ImageCarousel from './carousel';
+
+import location from './images/location.jpg';
+import './style.scss';
 
 const Home = () => {
 
   return (
     <div>
       <ImageCarousel />
-      
+      <div
+        className='location-picture-container'
+      >
+        <img
+          alt='find location'
+          src={location}
+        />
+        <div className='location-overlay text'>매장 찾기</div>
+        <Button variant='secondary' className='location-overlay button'>
+          <div className='button-text'>
+            가자 <IoMdArrowForward />
+          </div>
+        </Button>
+      </div>
     </div>
   )
 }
